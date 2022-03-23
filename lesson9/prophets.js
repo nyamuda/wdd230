db5156a0265e24364d96d2b81d09bef212b51181
Pierce Nyamuda <ptnrlab@gmail.com>
2022-03-23 11:50:29 +0200

    prophets added

---
+++ lesson9/prophets.js
@@ -0,0 +1,42 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+
+
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        console.log(jsonObject); // temporary checking for valid response and data parsing
+        const prophets = jsonObject['prophets'];
+        prophets.forEach(displayProphets);
+    });
+
+
+
+function displayProphets(prophet) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let birth = document.createElement('p');
+    let birth_place = document.createElement('p');
+    let portrait = document.createElement('img');
+
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
+    birth.innerHTML = `Date of Birth: ${prophet.birthdate}`
+    birth_place.innerHTML = `Place of Birth: ${prophet.birthplace}`
+
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`);
+    portrait.setAttribute('loading', 'lazy');
+
+    card.appendChild(h2);
+    card.appendChild(birth);
+    card.appendChild(birth_place)
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+
+
+}
