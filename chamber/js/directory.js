0634f3cd543e6730e74324e7e7716fc0246c104f
Pierce Nyamuda <ptnrlab@gmail.com>
2022-03-24 16:45:56 +0200

    last item added

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -2,6 +2,7 @@ let cardIcon = document.getElementById("card-icon");
 let listIcon = document.getElementById("list-icon");
 let cardContainer = document.getElementById("card");
 let listContainer = document.getElementById("list");
+let theTable = document.getElementById("table");
 
 
 cardIcon.addEventListener('click', () => {
@@ -16,11 +17,64 @@ listIcon.addEventListener('click', () => {
 })
 
 
-fetch("https://github.com/pnyamuda/wdd230/blob/main/chamber/data.json", {
-        mode: 'no-cors'
-    })
+fetch("https://raw.githubusercontent.com/pnyamuda/wdd230/main/chamber/data.json")
     .then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
+        return response.json()
+    }).then(info => {
+        info.data.forEach(element => {
+            //CARD CREATION
+            let card = document.createElement("div");
+            card.classList.add("card");
+            let image = document.createElement("img");
+            image.classList.add("card-img");
+            image.src = element.img;
+            image.alt = "logo"
+            let address = document.createElement("p");
+            address.classList.add("card-address");
+            address.innerHTML = element.address;
+            let phone = document.createElement("p");
+            phone.classList.add("card-phone");
+            phone.innerHTML = element.phone
+            let website = document.createElement("p");
+            website.classList.add("card-website");
+            website.innerHTML = element.website;
+
+
+            card.appendChild(image);
+            card.appendChild(address);
+            card.appendChild(phone);
+            card.appendChild(website);
+
+            cardContainer.appendChild(card);
+
+
+
+            //LIST CREATION
+
+            let tableRow = document.createElement("tr");
+            let tableDataName = document.createElement("td");
+            tableDataName.innerHTML = element.name;
+            let tableDataAddress = document.createElement("td");
+            tableDataAddress.innerHTML = element.address;
+            let tableDataPhone = document.createElement("td");
+            tableDataPhone.innerHTML = element.phone;
+            let tableDataWebsite = document.createElement("td");
+            let link = document.createElement("a");
+            link.innerHTML = element.website;
+            link.setAttribute("href", element.website);
+            tableDataWebsite.appendChild(link);
+
+            tableRow.appendChild(tableDataName);
+            tableRow.appendChild(tableDataAddress);
+            tableRow.appendChild(tableDataPhone);
+            tableRow.appendChild(tableDataWebsite);
+
+            theTable.appendChild(tableRow);
+
+
+
+
+
+
+        });
     })
