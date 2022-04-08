688749f62799bf215ee8ad192b32dc9a95bb27ee
Pierce Nyamuda <ptnrlab@gmail.com>
2022-04-08 12:54:01 +0200

    last modified added

--- tampleInn/js/hotel.js
+++ tampleInn/js/hotel.js
@@ -1,13 +1,12 @@
 let join = () => window.location.href = "reservation.html";
-
-
+let lastModified1 = document.querySelector("#last-modified1");
 let menu = document.getElementsByClassName('mobile-menu')
 
 let links = document.getElementsByClassName('mobile-links')
 let display = false
 
 
-
+lastModified1.innerHTML = document.lastModified;
 menu[0].onclick = () => {
     links[0].style.display = (!display) ? "flex" : "none"
     display = !display
